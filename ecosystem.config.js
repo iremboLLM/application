880b8d598c4792ecbce5196f1ae5bccaff7dcc64
dirac156 <dmurairimukongya@gmail.com>
2024-11-30 11:44:04 +0200

    run pm2 when deployed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,16 @@ module.exports = {
   apps: [
     {
       name: "irembo-frontend",
-      script: "npm",
-      args: "run start-prod:frontend",
-      //   cwd: "./frontend", // Set the working directory
+      script: "yarn",
+      args: "start",
+      cwd: "./frontend", // Set the working directory
       watch: true, // Enable watch mode
       ignore_watch: ["node_modules", "logs"], // Ignore unnecessary folders
     },
     {
       name: "irembo-backend",
       script: "./start-prod.sh",
-      //   cwd: "./backend", // Set the working directory
+      cwd: "./backend", // Set the working directory
       interpreter: "bash", // Use bash for the script
       watch: true, // Enable watch mode
       ignore_watch: ["venv", "logs"], // Ignore unnecessary folders

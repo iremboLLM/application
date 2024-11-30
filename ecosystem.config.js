c791ae8f2c9ddb90a3d513086053ab276a27e8a8
dirac156 <dmurairimukongya@gmail.com>
2024-11-30 11:23:59 +0200

    added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "irembo-frontend",
+      script: "npm",
+      args: "run start-prod:frontend",
+      cwd: "./frontend", // Set the working directory
+      watch: true, // Enable watch mode
+      ignore_watch: ["node_modules", "logs"], // Ignore unnecessary folders
+    },
+    {
+      name: "irembo-backend",
+      script: "./start-prod.sh",
+      cwd: "./backend", // Set the working directory
+      interpreter: "bash", // Use bash for the script
+      watch: true, // Enable watch mode
+      ignore_watch: ["venv", "logs"], // Ignore unnecessary folders
+    },
+  ],
+};
